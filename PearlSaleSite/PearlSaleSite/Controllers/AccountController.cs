da152fc798156dee14b08e2ac1d38e97033815fd
jing-andy <jing_andy@hotmail.com>
2014-12-18 01:07:50 -0800

    add model
    Add model

--- PearlSaleSite/PearlSaleSite/Controllers/AccountController.cs
+++ PearlSaleSite/PearlSaleSite/Controllers/AccountController.cs
@@ -7,7 +7,7 @@ using System.Web.Routing;
 using System.Web.Security;
 using PearlSaleSite.Models;
 
-namespace PearlSaleSite.Controllers
+namespace Pearl.Sale.Controllers
 {
     public class AccountController : Controller
     {
---
+++ PearlSaleSite/PearlSaleSite/Models/ShipInputModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Pearl.Sale.Models
+{
+
+    public class ShipInputModel
+    {
+        public List<KeyValuePair<string, string>> Warehouses { get; set; }
+        public List<KeyValuePair<string, string>> Currencies { get; set; }
+        public List<KeyValuePair<string, string>> Status { get; set; }
+        public List<KeyValuePair<string, string>> Shippers { get; set; }
+        public List<KeyValuePair<string, string>> Brands { get; set; }
+    }
+}
